a38873642d965f2b5cce3442e6a1a70b4a37f6af
Safiullah Khan <sarkaar836@gmail.com>
2025-09-12 00:26:11 +0100

    module done

---
+++ day_4_beginner_Randomisation_and_python_list/task_1_Random_Module.py
@@ -0,0 +1,17 @@
+import random
+# import my_module
+# a = random.randint(1, 99)
+# b = random.randint(1, 88)
+# print(a + b)
+# print(my_module.my_fav_number)
+
+# a = random.random()
+# print (a)
+print("welcome to the coin toss program")
+a = random.randint(0,1)
+choose = input("choose ")
+
+if a == 1:
+  print("heads ")
+else:
+  print("tails")  
