0f5a71e56a84f3ebd56b51c3cb1b6c76607b30e0
Safiullah Khan <sarkaar836@gmail.com>
2025-09-15 03:45:34 +0100

    almost there

---
+++ day_4_beginner_Randomisation_and_python_list/task_4_who_will_pay_the_bill.py
@@ -0,0 +1,3 @@
+import random
+friends = ["Angela", "Ben", "Jenny", "Michael", "Chloe"]
+print(random.choice(friends))
---
+++ day_4_beginner_Randomisation_and_python_list/task_5_indexerrors_&_woking_with_nested_list.py
@@ -0,0 +1,1 @@
+dirty_dozen = ["Strawberries", "Spinach", "Kale", "Nectarines", "Apples", "Grapes"]
